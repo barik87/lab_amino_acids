a858df2ed02cd31f3146d4e9e3032040e138a82a
Maksym Barvinskyi <Maksym_Barvinskyi@epam.com>
2017-03-09 19:26:29 +0200

    Initial commit

---
+++ dictionaries.py
@@ -0,0 +1,119 @@
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+from openpyxl import load_workbook
+
+MULTIPLIERS = dict(
+    Plazma=dict(
+        Alanine=325.9346088,
+        Methionine=325.747619,
+        Isoleucine=343.4544872,
+        Lysine=187.0718163,
+        Proline=724.7446809
+    ),
+    Urine=dict(
+        Alanine=325.6844011,
+        Methionine=321.8833333,
+        Isoleucine=335.89375,
+        Lysine=187.6297872,
+        Proline=724.7446809
+    )
+)
+
+CONST_MULTIPLIERS = {
+    'Alanine': 2925.907,
+    'Methionine': 3003.707,
+    'Isoleucine': 2967.626,
+    'Lysine': 940.3833,
+    'Proline': 2873.455,
+    'Aspartic acid': 2222.763,
+    'Glutamic acid': 2174.47,
+    'Serine': 2557.236,
+    'Histidine': 1697.498,
+    'Glycine': 2800.287,
+    'Threonine': 2606.218,
+    'Citruline': 2672.156,
+    'Arginine': 2614.298,
+    'Taurine': 3093.659,
+    'Tyrosine': 2634.504,
+    'Cystine': 246.4665,
+    'Valine': 2895.654,
+    'Phenylalanine': 2787.873,
+    'Leucine': 2951.304
+}
+
+UKR_MONTHS = {
+    1: 'січня',
+    2: 'лютого',
+    3: 'березня',
+    4: 'квітня',
+    5: 'травня',
+    6: 'червня',
+    7: 'липня',
+    8: 'серпня',
+    9: 'вересня',
+    10: 'жовтня',
+    11: 'листопада',
+    12: 'грудня'
+}
+
+
+def get_acids(data_file_name: str, an_type: str, sample_date: datetime, birth_date: datetime) -> (str, dict,):
+    diff_in_years = relativedelta(sample_date, birth_date).years
+    diff_in_months = relativedelta(sample_date, birth_date).months
+    if an_type == 'Plazma':
+        if diff_in_years >= 12:
+            return '12+ р.', get_acids_from_file(data_file_name, 13)
+        if diff_in_years > 0 or diff_in_months >= 3:
+            return '3м-12р', get_acids_from_file(data_file_name, 12)
+        if diff_in_months < 3:
+            return '0-3м.', get_acids_from_file(data_file_name, 11)
+        print('ERROR: check dates!')
+    else:
+        if diff_in_years >= 13:
+            return '13+ р.', get_acids_from_file(data_file_name, 9)
+        if diff_in_years >= 7:
+            return '7-13р.', get_acids_from_file(data_file_name, 8)
+        if diff_in_years >= 4:
+            return '4-7р.', get_acids_from_file(data_file_name, 7)
+        if diff_in_years >= 2:
+            return '2-4р.', get_acids_from_file(data_file_name, 6)
+        if diff_in_years >= 1:
+            return '1-2р.', get_acids_from_file(data_file_name, 5)
+        if diff_in_months >= 6:
+            return '6-12м.', get_acids_from_file(data_file_name, 4)
+        if diff_in_months >= 1:
+            return '1-6м.', get_acids_from_file(data_file_name, 3)
+        if diff_in_months == 0:
+            return '0-1м.', get_acids_from_file(data_file_name, 2)
+        print('ERROR: check dates!')
+
+
+def get_acids_from_file(data_file_name: str, column: int) -> dict:
+    result = dict()
+    wb = load_workbook(data_file_name)
+    ws = wb.get_sheet_by_name('Norms')
+    for row_idx in range(2, 21):
+        acid_name = str(list(ws.rows)[row_idx][0].value)
+        cell_values = str(list(ws.rows)[row_idx][column].value).split('--')
+        result[acid_name] = (cell_values[0], cell_values[1],)
+    return result
+
+
+def get_acids_list(data_file_name: str) -> list:
+    result = []
+    wb = load_workbook(data_file_name)
+    ws = wb.get_sheet_by_name('Norms')
+    for row_idx in range(2, 21):
+        result.append(str(list(ws.rows)[row_idx][0].value))
+    return result
+
+
+def get_parameters_from_file(data_file_name: str) -> dict:
+    result = {}
+    wb = load_workbook(data_file_name)
+    ws = wb.get_sheet_by_name('Parameters')
+    key = str(list(ws.rows)[1][0].value)
+    value = list(ws.rows)[1][1].value
+    result[key] = value
+    return result
---
+++ lab.py
@@ -0,0 +1,218 @@
+import glob
+
+from docx import Document
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.cell import Cell
+from openpyxl.styles import PatternFill
+
+import dictionaries
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--mult', dest='is_mult', action='store_const', const=True, default=False,
+                    help='whether to multiply some amino acids')
+parser.add_argument('--all-mult', dest='is_all_mult', action='store_const', const=True, default=False,
+                    help='whether to multiply all amino acids')
+args = parser.parse_args()
+
+DATA_FILE_NAME = 'data.xlsx'
+TEMPLATE_PLAZMA_FILE_NAME = 'template_plazma.docx'
+TEMPLATE_URINE_FILE_NAME = 'template_urine.docx'
+
+MULTS = dictionaries.MULTIPLIERS
+ALL_MULTS = dictionaries.CONST_MULTIPLIERS
+PARAMETERS = dictionaries.get_parameters_from_file(DATA_FILE_NAME)
+
+DATA = dict()
+print('Reading data from "%s"...' % DATA_FILE_NAME)
+wb = load_workbook(DATA_FILE_NAME)
+ws = wb.worksheets[0]
+headers = list(map(lambda row_cell: str(row_cell.value), list(ws.rows)[0]))
+for row_idx in range(1, len(list(ws.rows))):
+    row = list(ws.rows)[row_idx]
+    row_id = None
+    for col_idx in range(len(row)):
+        cell = row[col_idx]
+        if col_idx == 0:
+            row_id = str(cell.value)
+            DATA[row_id] = dict()
+        else:
+            DATA[row_id][headers[col_idx]] = cell.value
+print('Done.')
+
+green_fill = PatternFill("solid", fgColor='92D050')
+yellow_fill = PatternFill("solid", fgColor='FFC000')
+red_fill = PatternFill("solid", fgColor='FF0000')
+
+# create total_results.xlsx
+acid_list = dictionaries.get_acids_list(DATA_FILE_NAME)
+total_wb = Workbook()
+total_ws = total_wb.active
+acids_row = [''] + acid_list
+total_ws.append(acids_row)
+
+for file_name in glob.glob('*.xlsx'):
+    if 'results' not in file_name and file_name != DATA_FILE_NAME:
+        print('\nWorking with file "%s"...' % file_name)
+
+        file_id = file_name.split('.')[0]
+        data = DATA[file_id]
+
+        norm_age, acid_norms = dictionaries.get_acids(DATA_FILE_NAME, data['Type'], data['SampleTakeDate'],
+                                                      data['BirthDate'])
+
+        wb = load_workbook(file_name)
+        ws = wb.worksheets[0]
+
+        start_row_idx = -1
+        start_col_idx = -1
+        end_row_idx = -1
+        end_col_idx = -1
+        area_col_idx = -1
+        for row_idx in range(len(list(ws.rows))):
+            row = list(ws.rows)[row_idx]
+            if start_row_idx == -1:
+                for col_idx in range(len(row)):
+                    cell = row[col_idx]
+                    if str(cell.value) == 'Peak Name':
+                        start_row_idx = row_idx
+                        start_col_idx = col_idx
+                    if str(cell.value).strip() == 'Area':
+                        area_col_idx = col_idx
+                    if str(cell.value).strip() == 'Amount':
+                        end_col_idx = col_idx
+                        break
+            elif row_idx == len(list(ws.rows)) - 1:
+                end_row_idx = row_idx
+            else:
+                for col_idx in range(len(row)):
+                    cell = row[col_idx]
+                    if row_idx > start_row_idx + 2 and col_idx == end_col_idx and cell.value is None:
+                        end_row_idx = row_idx - 1
+                        break
+            if start_row_idx > -1 and start_col_idx > -1 and end_row_idx > -1 and end_col_idx > -1:
+                break
+        else:
+            print('ERROR: Cell with text "Peak Name" not found in the document.')
+
+        amounts = dict()
+        total_row = [data['Name']] + [''] * len(acid_list)
+        new_wb = Workbook()
+        new_ws = new_wb.active
+        for row_idx in range(start_row_idx, end_row_idx + 1):
+            row = list(ws.rows)[row_idx]
+            acid_name = row[start_col_idx].value
+            if acid_name is not None and acid_name != '':
+                new_row = []
+                for col_idx in range(start_col_idx, end_col_idx + 1):
+                    cell = row[col_idx]
+                    value = '' if cell.value is None else cell.value
+                    if col_idx == end_col_idx:
+                        if args.is_all_mult and row_idx > start_row_idx + 2:
+                            try:
+                                value = float(row[area_col_idx].value) / ALL_MULTS[str(row[start_col_idx].value)]
+                            except ValueError:
+                                raise Exception('counts*min is not number for %s' % str(row[start_col_idx].value))
+                        elif value == 'n.a.':
+                            if row[area_col_idx].value != 'n.a.':
+                                if args.is_mult:
+                                    value = row[area_col_idx].value * MULTS[data['Type']][str(row[start_col_idx].value)]
+                                else:
+                                    value = row[area_col_idx].value
+                            else:
+                                value = 0
+                        if type(value) in [int, float]:
+                            value = value * PARAMETERS['Multiplier']
+                        if data['Type'] == 'Urine' and value != 'n.a.' and row_idx > start_row_idx + 2:
+                            try:
+                                value = float(value) / float(data['Creatinine'])
+                            except TypeError:
+                                print('ERROR: Creatinine value for ID %s is not number.' % file_id)
+                        amounts[acid_name] = value
+                        if acid_name in acid_norms:
+                            new_cell = Cell(new_ws)
+                            new_cell.value = value
+                            total_cell = Cell(total_ws)
+                            total_cell.value = value
+
+                            lbound, rbound = acid_norms[acid_name]
+                            if float(value) < float(lbound):
+                                new_cell.fill = yellow_fill
+                                total_cell.fill = yellow_fill
+                            elif float(value) <= float(rbound):
+                                new_cell.fill = green_fill
+                                total_cell.fill = green_fill
+                            else:
+                                new_cell.fill = red_fill
+                                total_cell.fill = red_fill
+
+                            new_row.append(new_cell)
+                            new_row.append('%s--%s' % (lbound, rbound))
+                            total_row[1 + acid_list.index(acid_name)] = total_cell
+                        else:
+                            new_row.append(value)
+                    else:
+                        new_row.append(value)
+                new_ws.append(new_row)
+        new_file_name = file_id + '_results.xlsx'
+        new_wb.save(filename=new_file_name)
+        print('File %s generated.' % new_file_name)
+
+        total_ws.append(total_row)
+
+        temp_doc_name = TEMPLATE_PLAZMA_FILE_NAME if data['Type'] == 'Plazma' else TEMPLATE_URINE_FILE_NAME
+        document = Document(temp_doc_name)
+        for paragraph in document.paragraphs:
+            for run in paragraph.runs:
+                if 'FULLNAME' in run.text:
+                    run.text = str(run.text).replace('FULLNAME', data['Name'])
+                if 'DDMMYYYY' in run.text:
+                    run.text = str(run.text).replace('DDMMYYYY', data['BirthDate'].strftime('%d.%m.%Y'))
+                if 'SAMPLEDAY' in run.text:
+                    run.text = str(run.text).replace('SAMPLEDAY', data['SampleTakeDate'].strftime('%d'))
+                if 'SAMPLEYEAR' in run.text:
+                    run.text = str(run.text).replace('SAMPLEYEAR', data['SampleTakeDate'].strftime('%Y'))
+                if 'SAMPLEMONTH' in run.text:
+                    run.text = str(run.text).replace('SAMPLEMONTH',
+                                                     dictionaries.UKR_MONTHS[data['SampleTakeDate'].month])
+                if 'ANDAY' in run.text:
+                    run.text = str(run.text).replace('ANDAY', data['AnalysisDate'].strftime('%d'))
+                if 'ANYEAR' in run.text:
+                    run.text = str(run.text).replace('ANYEAR', data['AnalysisDate'].strftime('%Y'))
+                if 'ANMONTH' in run.text:
+                    run.text = str(run.text).replace('ANMONTH',
+                                                     dictionaries.UKR_MONTHS[data['AnalysisDate'].month])
+
+        table = document.tables[0]
+        for col_idx in range(3, 5):
+            for p in table.cell(0, col_idx).paragraphs:
+                for r in p.runs:
+                    if r.text == 'BOUND':
+                        r.text = norm_age
+
+        for row_idx in range(1, len(table.rows)):
+            acid_name = acid_list[row_idx - 1]
+            lbound, rbound = acid_norms[acid_name]
+
+            amount = float(amounts[acid_name])
+            suffix = ''
+            # if amount < float(lbound):
+            # suffix = '↓'
+            # elif amount > float(rbound):
+            #     suffix = '↑'
+
+            str_amount = ('0' if amounts[acid_name] == 0 else '%.2f' % amount) + suffix
+            str_amount = str_amount.replace('.', ',')
+            table.cell(row_idx, 1).paragraphs[0].runs[0].text = str_amount
+            table.cell(row_idx, 3).paragraphs[0].runs[0].text = str(lbound)
+            table.cell(row_idx, 4).paragraphs[0].runs[0].text = str(rbound)
+
+        new_doc_file_name = '_'.join(list(data['Name'].split(' ')) + [data['Type']]) + '.docx'
+        document.save(new_doc_file_name)
+        print('File docx generated.')
+
+total_file_name = 'total_results.xlsx'
+total_wb.save(filename=total_file_name)
+print('\nFile %s generated.' % total_file_name)
